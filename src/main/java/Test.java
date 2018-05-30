480d549c07dcc2c26bb42551f36e383159a999c6
yangtao <yang_tao@letzgo.com.cn>
2018-05-30 15:57:02 +0800

    add submodule

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,3 @@
-import com.visionet.core.redis.RedisUtil;
-import com.visionet.core.util.AESUtil2;
-import com.visionet.core.util.PasswordUtil;
-import com.visionet.user.dome.CarWorkDto;
-
-import java.util.List;
-import java.util.Random;
-
 /**
  * Author   yang_tao@<yangtao.letzgo.com.cn>
  * Date     2018-01-22 17:20
@@ -13,17 +5,7 @@ import java.util.Random;
  */
 public class Test {
     public static void main(String[] args) {
-//        System.out.println(AESUtil2.encode("7674&18916129167&2018-03-04 13:07:56"));
-//        System.out.println(AESUtil2.decode("C8D0B0F92708A96FC7D2CB6940D10212580758E5A07B019F1BBA3ECF5A3B2D6C6BDAEF066BF9765724B78D92261DD421"));
-//        System.out.println(PasswordUtil.Md5Enerate("1234567", true));
-//        CarWorkDto carWorkDto = new CarWorkDto();
-//        carWorkDto.setCid(1);
-//        carWorkDto.setPhone("32252");
-//        RedisUtil.setObjectListData("test", carWorkDto);
-//        List<Object> test = RedisUtil.getObjectListData("test");
-//        System.out.println(test);
-
-        System.out.println("jmhdfmkgm；富商大贾的\nfsdfg返还冻结很烦改".replaceAll("\n",""));
+        System.out.println("hello world");
     }
 
 }
